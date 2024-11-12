15d2e6c77daa7b06fff92dcb20a1de0dfaa38d7d
sriram2601 <127611008+sriram1015@users.noreply.github.com>
2024-11-12 23:49:59 +0530

    userActions.js

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import {message} from 'antd'
-
+const apiUrl = process.env.REACT_APP_API_URL ;
 export const userLogin=(reqObj)=>async dispatch=>{
     
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-        const response = await axios.post('http://localhost:5000/api/users/login' , reqObj)
+        const response = await axios.post(`${apiUrl}/api/users/login` , reqObj)
         localStorage.setItem('user' , JSON.stringify(response.data))
         message.success('Login success')
         dispatch({type: 'LOADING' , payload:false})
@@ -26,7 +26,7 @@ export const userRegister=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-        const response = await axios.post('http://localhost:5000/api/users/register' , reqObj)
+        const response = await axios.post(`${apiUrl}/api/users/register` , reqObj)
         message.success('Registration successfull')
         setTimeout(() => {
             window.location.href='/login'
